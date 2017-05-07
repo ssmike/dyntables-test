65ae6735fe0000ad5e70fc40bd27a9ecc6077a8d
ssmike <ssmike@yandex-team.ru>
2017-05-07 19:35:19 +0300

    fix setup script

--- control/control/setup-conf.sh
+++ control/control/setup-conf.sh
@@ -2,8 +2,9 @@
 cd /root/
 git clone https://github.com/ssmike/dotfiles config
 cd config
+git checkout mini
 git submodule update --init
 ./make-symlinks.sh
 chsh root -s /bin/zsh
-vim -c ":BundleUpdate"
-rm -r /root/.vim/bundle/YouCompleteMe/
+#vim -c ":BundleUpdate"
+#rm -r /root/.vim/bundle/YouCompleteMe/
